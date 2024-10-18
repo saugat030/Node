133aedb448ca28e2995430dd769e499ebaf847d2
ZuluMumu <saugatdahal030@gmail.com>
2024-10-18 08:25:23 +0545

    npmStuffs

---
+++ 10-http.js
@@ -0,0 +1,18 @@
+const http = require('http');
+//request and response are both build in objects that the createServer function can take as a paramater.
+const server = http.createServer((req , res)=>{
+    //console.log(req);
+    if(req.url === '/'){
+    res.write('Welcome to Nigga clan Home page.');
+    res.end();
+    }
+    else if(req.url ==='/about'){
+        res.write('Welcome to ABOUTTTTTTTTTTTTTT');
+        res.end();
+    }
+    else {
+        res.write('<h1>Xaina esto kei page.</h1><p>This page does not exist. You can return back to home page by clicking the button below: </p><a href="/">here</a>');
+        res.end();
+    }
+ })
+ server.listen(5000);
